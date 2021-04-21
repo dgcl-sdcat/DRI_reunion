46e151d16a8446186756e2f961f0ac956f523ce8
dgcl-sdcat <80058755+dgcl-sdcat@users.noreply.github.com>
2021-04-21 17:15:02 +0200

    Add files via upload

---
+++ data/ProjetRnovationthermique_2.js
@@ -0,0 +1,8 @@
+var json_ProjetRnovationthermique_2 = {
+"type": "FeatureCollection",
+"name": "ProjetRnovationthermique_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Thème": "Rénovation thermique des bâtiments publics", "Projet": "Rénovation de l'internat du lycée Rolland Garos du Tampon", "Coût": "2 600 000 €", "Subvention": "2 080 000 €" }, "geometry": { "type": "Point", "coordinates": [ 55.51924197, -21.27369931 ] } }
+]
+}
---
+++ data/ProjetsMobilitTransportscollectifs_3.js
@@ -0,0 +1,12 @@
+var json_ProjetsMobilitTransportscollectifs_3 = {
+"type": "FeatureCollection",
+"name": "ProjetsMobilitTransportscollectifs_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Thème": "Mobilité – transports collectifs", "Projet": "Le shunt rivière des galets", "Coût": "1 500 000 €", "Subvention": "1 200 000 €" }, "geometry": { "type": "Point", "coordinates": [ 55.30717633, -20.96005128 ] } },
+{ "type": "Feature", "properties": { "Thème": "Mobilité – transports collectifs", "Projet": "La Passerelle Ravine Ste Marguerite sur la RN2 à Saint Benoit", "Coût": "1 600 000 €", "Subvention": "1 250 000 €" }, "geometry": { "type": "Point", "coordinates": [ 55.72686645, -21.05376082 ] } },
+{ "type": "Feature", "properties": { "Thème": "Mobilité – transports collectifs", "Projet": "RN3 - La Confiance, Chemin de ceinture, création d'un accotement multifonctionnel", "Coût": "420 000 €", "Subvention": "333 000 €" }, "geometry": { "type": "Point", "coordinates": [ 55.69762753, -21.07765408 ] } },
+{ "type": "Feature", "properties": { "Thème": "Mobilité – transports collectifs", "Projet": "RN2 - Aménagement piétons cycles Vincendo\/ St Joseph (en agglomération)", "Coût": "3 200 000 €", "Subvention": "2 600 000 €" }, "geometry": { "type": "Point", "coordinates": [ 55.63585656, -21.37980732 ] } },
+{ "type": "Feature", "properties": { "Thème": "Mobilité – transports collectifs", "Projet": "Etagn Salé à Saint Louis - Voie vélo régionale", "Coût": "1 400 000 €", "Subvention": "1 100 000 €" }, "geometry": { "type": "Point", "coordinates": [ 55.37681465, -21.23625587 ] } }
+]
+}
